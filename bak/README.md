5e727242c96cafdac02df72ccbb1aa15056eab19
施艳 <shiyan@datatom.com>
2023-03-02 17:06:45 +0800

    换部署方式

---
+++ bak/README.md
@@ -0,0 +1,3 @@
+# [Lazarus 的博客](https://melindasssss.github.io/)
+
+![Lazarus的博客](./home.png 'Lazarus的博客')
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 const HomePage = () => {
   return (
     <div>
+      <br />
+      <br />
+      <br />
       <h1>人活三万天，开心一天是一天</h1>
+      <br />
+      <br />
+      <br />
+      <h2>你好，我是MelindaSssss</h2>
     </div>
   )
 }
