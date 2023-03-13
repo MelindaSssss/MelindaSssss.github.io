31eaf4c88db1f8bdd992c650b4fc176d4ff9b3a4
施艳 <shiyan@datatom.com>
2023-03-13 18:00:06 +0800

    feat: 左侧菜单

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -1,15 +1,40 @@
-import { Watermark } from 'antd'
+// import { Watermark } from 'antd'
+
+import { useState } from 'react'
+import { Layout, Menu } from 'antd'
+import type { MenuProps } from 'antd'
+import { items } from './data'
+import './index.css'
+
+const { Header, Footer, Sider, Content } = Layout
+
 const WorkPage = () => {
+  const [pageType, setPageType] = useState<string | 'home'>('home')
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e)
+    setPageType(e.key)
+  }
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <Watermark content="MelindaSssss-Work">
-        <div>工作页面</div>
-        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{' '}
-        <br /> <br /> <br /> <br />
-        <p>工作页面</p>
-        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{' '}
-        <br /> <br /> <br /> <br />
-      </Watermark>
+    <div className="work-page">
+      <Layout>
+        <Sider className="work-sider" width={250}>
+          <div className="sider-top">好好工作，努力拼搏</div>
+          <div className="sider-bottom">
+            <Menu onClick={onClick} mode="inline" items={items} />
+          </div>
+        </Sider>
+        <Layout>
+          {pageType === 'home' ? (
+            <>
+              <Header>Header</Header>
+              <Content>Content</Content>
+              <Footer>Footer</Footer>
+            </>
+          ) : (
+            <div>展示文章详情</div>
+          )}
+        </Layout>
+      </Layout>
     </div>
   )
 }
