c2dd8621d20ff4fc468336aae4651827e4c8d4cd
施艳 <shiyan@datatom.com>
2023-04-04 17:09:37 +0800

    feat: 补充skilllist

--- src/pages/Introduction/data.ts
+++ src/pages/Introduction/data.ts
@@ -1,5 +1,13 @@
 const SKILL_LIST = [
   {
+    bg: 'red',
+    desc: '女孩子',
+  },
+  {
+    bg: 'pink',
+    desc: '前端',
+  },
+  {
     bg: '#f61177',
     desc: 'base南京',
   },
@@ -13,11 +21,63 @@ const SKILL_LIST = [
   },
   {
     bg: 'orange',
-    desc: 'node',
+    desc: 'nodejs',
   },
   {
     bg: 'blue',
     desc: 'typescript',
+  },
+  {
+    bg: 'orange',
+    desc: 'tensorflow/tfjs',
+  },
+  {
+    bg: 'purple',
+    desc: 'python',
+  },
+  {
+    bg: 'green',
+    desc: 'linux',
+  },
+  {
+    bg: 'blue',
+    desc: 'webpack',
+  },
+  {
+    bg: 'orange',
+    desc: 'vue',
+  },
+  {
+    bg: '#9c27b0',
+    desc: 'mysql',
+  },
+  {
+    bg: 'red',
+    desc: '荣耀王者(单排)',
+  },
+  {
+    bg: 'rgb(255, 0, 140)',
+    desc: '大师(单排)',
+  },
+  {
+    bg: 'rgb(123, 12, 140)',
+    desc: '厨神',
+  },
+  {
+    bg: 'blue',
+    desc: '7年驾龄',
+  },
+  {
+    bg: '#9c27b0',
+    desc: '画画(白描)',
+  },
+  {
+    bg: 'rgb(255, 234, 0)',
+    desc: '英语',
+  },
+  {
+    bg: '#27b039',
+    desc: '日语',
   },
 ]
 
