c4e67b80e3722108fe1cc6c9fd8eacd00387cf72
施艳 <shiyan@datatom.com>
2023-04-18 17:47:07 +0800

    feat: 调整introduction页面展示

--- src/pages/Introduction/index.tsx
+++ src/pages/Introduction/index.tsx
@@ -1,9 +1,9 @@
-import { Layout, Badge } from 'antd'
+import { Badge } from 'antd'
 import type { pageTypeProps } from '../../App'
 import CommonFooter from '../../components/Footer'
 import './style.css'
 import { SKILL_LIST } from './data'
-const { Header, Content, Footer, Sider } = Layout
+
 type IntroductionPageProps = {
   setPageType: React.Dispatch<React.SetStateAction<pageTypeProps>>
 }
@@ -11,17 +11,13 @@ const IntroductionPage = (props: IntroductionPageProps) => {
   const { setPageType } = props
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <Layout>
-        <Header style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ margin: '0 40px' }}>
+        {/* header */}
+        <div
+          className="header-box"
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
           <h2>:::::::::::::::</h2>
-          {/* <div className="button-wrap">
-            <span
-              onClick={() => setPageType(null)}
-              className="beautiful-button"
-            >
-              返 回
-            </span>
-          </div> */}
           {/* 特殊小鸟按钮 */}
           <div
             className="button button--bird"
@@ -44,50 +40,45 @@ const IntroductionPage = (props: IntroductionPageProps) => {
               </div>
             </div>
           </div>
-        </Header>
-        <Content style={{ height: 'calc( 100% - 100px)' }}>
-          <Layout style={{ padding: '24px 0' }}>
-            <Sider width={200} style={{ paddingLeft: 50 }}>
-              {SKILL_LIST.map((item: typeof SKILL_LIST[0]) => {
-                return (
-                  <div>
-                    <Badge
-                      className="site-badge-count-109"
-                      count={item.desc}
-                      style={{ backgroundColor: item.bg }}
-                    />
-                  </div>
-                )
-              })}
-            </Sider>
-            <Content style={{ padding: '0 24px' }}>
-              {/* <h2>自 白 书</h2>
-              <div>
-                如果我明天死去，我的墓志铭可以这样写 ----
-                她拥有二十七年峥嵘岁月，和一位名叫点点的年轻爱人。不过如果运气好的话，也可以是她拥有百年峥嵘岁月，和一个叫做点点的老头儿。
-              </div> */}
-
-              <div className="content-wrapper-pink">
-                {/* before */}
-                <div className="content-inner-pink">
-                  <div className="content-title-pink">
-                    {' '}
-                    <h2>自 白 书</h2>
-                  </div>
-                  <div className="content-desc-pink">
-                    如果我明天死去，我的墓志铭可以这样写 ----
-                    她拥有二十七年峥嵘岁月，和一位名叫点点的年轻爱人。不过如果运气好的话，也可以是她拥有百年峥嵘岁月，和一个叫做点点的老头儿。
-                  </div>
+        </div>
+        {/* content */}
+        <div className="content-box" style={{ display: 'flex' }}>
+          <div className="content-left">
+            {SKILL_LIST.map((item: typeof SKILL_LIST[0]) => {
+              return (
+                <div>
+                  <Badge
+                    className="site-badge-count-109"
+                    count={item.desc}
+                    style={{ backgroundColor: item.bg }}
+                  />
                 </div>
-                {/* after */}
+              )
+            })}
+          </div>
+          <div className="content-right">
+            <div className="content-wrapper-pink">
+              {/* before */}
+              <div className="content-inner-pink">
+                <div className="content-title-pink">
+                  <h2>自 白 书</h2>
+                </div>
+                <div className="content-desc-pink">
+                  如果我明天死去，我的墓志铭可以这样写 ----
+                  她拥有二十七年峥嵘岁月，和一位名叫点点的年轻爱人。不过如果运气好的话，也可以是她拥有百年峥嵘岁月，和一个叫做点点的老头儿。
+                </div>
               </div>
-            </Content>
-          </Layout>
-        </Content>
-        <Footer>
+              {/* after */}
+            </div>
+          </div>
+        </div>
+        {/* main-introduction */}
+        <div className="main-introduction-box"></div>
+        {/* footer */}
+        <div className="footer-box">
           <CommonFooter />
-        </Footer>
-      </Layout>
+        </div>
+      </div>
     </div>
   )
 }
