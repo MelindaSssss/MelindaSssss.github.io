b595e60a98d1573aaed2fb44bca32a1225c26bcd
施艳 <shiyan@datatom.com>
2023-07-20 10:00:21 +0800

    feat: 领袖用典

--- src/pages/Life/index.tsx
+++ src/pages/Life/index.tsx
@@ -25,7 +25,7 @@ const LifePage = (props: LifeType) => {
   }
   return (
     <div className="special-wrap-life">
-      <Layout className='life-inner'>
+      <Layout className="life-inner">
         <Sider
           // style={{
           //   overflow: 'auto',
@@ -51,7 +51,7 @@ const LifePage = (props: LifeType) => {
             <Menu onClick={onClick} mode="inline" items={items} />
           </div>
         </Sider>
-        <Layout style={{ width: '100%', height: '100%' }}>
+        <Layout style={{ width: '100%', height: '100%', color: 'lightblue' }}>
           {pageType === 'home' ? (
             <>
               <Header>
